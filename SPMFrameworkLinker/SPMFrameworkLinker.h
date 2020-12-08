28ab04509eba015d05602314dada942500d505f2
Atom <fromatom@gmail.com>
2020-12-08 17:38:00 +0900

    add RxSwift

---
+++ SPMFrameworkLinker/SPMFrameworkLinker.h
@@ -0,0 +1,18 @@
+//
+//  SPMFrameworkLinker.h
+//  SPMFrameworkLinker
+//
+//  Created by FromAtom on 2020/12/08.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SPMFrameworkLinker.
+FOUNDATION_EXPORT double SPMFrameworkLinkerVersionNumber;
+
+//! Project version string for SPMFrameworkLinker.
+FOUNDATION_EXPORT const unsigned char SPMFrameworkLinkerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SPMFrameworkLinker/PublicHeader.h>
+
+
